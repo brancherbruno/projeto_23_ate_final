0b92202b2a35c24cf522352a1b29c3735b4df187
Bruno Brancher <brunodesouzabrancher@gmail.com>
2023-01-22 12:33:08 -0300

    exercício de def para saber total da multiplicação e se é par ou ímpar

---
+++ .vscode/exe_113.py
@@ -0,0 +1,11 @@
+# Exercícios com funções
+
+# Crie uma função que multiplica todos os argumentos
+# não nomeados recebidos
+# Retorne o total para uma variável e mostre o valor
+# da variável.
+
+
+
+
+
