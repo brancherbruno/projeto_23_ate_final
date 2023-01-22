288505983942d46982768c03b992ed1f364238f1
Bruno Brancher <brunodesouzabrancher@gmail.com>
2023-01-22 12:33:55 -0300

    exercício de def para saber total da multiplicação e se é par ou ímpar

--- .vscode/aula_110.py
+++ .vscode/aula_110.py
@@ -19,3 +19,38 @@ print(soma2)
 print(soma(11, 55))
 
 
+"""
+args - Argumentos não nomeados
+* - *args (empacotamento e desempacotamento)
+"""
+# Lembre-te de desempacotamento
+# x, y, *resto = 1, 2, 3, 4
+# print(x, y, resto)
+
+
+# def soma(x, y):
+#     return x + y
+
+def soma(*args):
+    total = 0
+    for numero in args:
+        total += numero
+    return total
+
+
+soma_1_2_3 = soma(1, 2, 3)
+# print(soma_1_2_3)
+
+soma_4_5_6 = soma(4, 5, 6)
+# print(soma_4_5_6)
+
+numeros = 1, 2, 3, 4, 5, 6, 7, 78, 10
+outra_soma = soma(*numeros)
+print(outra_soma)
+
+print(sum(numeros))
+# print(*numeros)
+# o *args vai empacotar os itens em uma tupla
+# para desempacotar para poder realizar procedimentos como por exemplo, soma, divissao, etc, devo usar *nome_da_variável - o 
+# asterisco vai desempacotar oque o *args empacotou liberando para realizar os procedimentos.
+
--- .vscode/exe_113.py
+++ .vscode/exe_113.py
@@ -6,6 +6,28 @@
 # da variável.
 
 
+def multi(*args):
+    total = 1
+    for i in args:
+        total *= i
+    return total
+multiplicacao = multi(2, 5, 50)
+print(multiplicacao)
+
+# Crie uma função fala se um número é par ou ímpar.
+# Retorne se o número é par ou ímpar.
+
+def par_impar(numero):
+    valor = numero % 2 == 0
+    if valor:
+        return f"{numero} é par."
+    return f"{numero} é ímpar."
+total = par_impar(68)
+print(total)
+
+
+
+
 
 
 
