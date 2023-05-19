6417fd0590c3c6053e076f247ad0ea327489f2b4
Bruno Brancher <brunodesouzabrancher@gmail.com>
2023-05-18 21:48:09 -0300

    aula 153 ate 155 - modulos e mais

---
+++ .vscode/aula_154_m.py
@@ -0,0 +1,8 @@
+print('hello world do modulo', __name__)
+
+variavel_modulo = 'Importação de modulo com sucesso'
+
+
+def soma(x, y):
+    return x + y
+
---
+++ .vscode/aula_155.py
@@ -0,0 +1,12 @@
+import aula_154_m
+from aula_154_m import soma, variavel_modulo
+
+print('Este módulo se chama', __name__)
+# print('Este módulo se chama', __name__)
+print(aula_154_m.variavel_modulo)
+print(variavel_modulo)
+print(soma(2, 3))
+print(aula_154_m.soma(2, 3))
+
+
+
